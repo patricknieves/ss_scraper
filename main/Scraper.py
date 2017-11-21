c2bb6ddc86adc97b2580060a0e9ad94e87c9d78f
Patrick Nieves <nieves_p@hotmail.de>
2017-11-21 11:36:45 +0100

    str fix

--- main/Scraper.py
+++ main/Scraper.py
@@ -302,7 +302,7 @@ def get_bitcoin_transaction(new_exchanges):
                                     change_ip()
                                     for attempt in range(5):
                                         try:
-                                            fee_from = requests.get("https://api.blockcypher.com/v1/btc/main/txs/" + transaction["hash"]).json()["fees"] / 100000000
+                                            fee_from = requests.get("https://api.blockcypher.com/v1/btc/main/txs/" + str(transaction["hash"])).json()["fees"] / 100000000
                                         except:
                                             change_ip()
                                         else:
@@ -407,16 +407,16 @@ def search_corresponding_transaction(currency, tx_hash, exchange_id):
     for attempt in range(5):
         try:
             if currency == "ETH":
-                transaction = requests.get("https://etherchain.org/api/tx/" + tx_hash).json()["data"][0]
+                transaction = requests.get("https://etherchain.org/api/tx/" + str(tx_hash)).json()["data"][0]
                 cur.execute("UPDATE exchanges SET  time_to = %s, fee_to = %s WHERE id = %s", (transaction["time"].replace("T"," ")[:-5], (transaction["gasUsed"]*(transaction["price"]/ 1E+18)), exchange_id))
             elif currency == "BTC":
-                transaction = requests.get("https://api.blockcypher.com/v1/btc/main/txs/" + tx_hash).json()
+                transaction = requests.get("https://api.blockcypher.com/v1/btc/main/txs/" + str(tx_hash)).json()
                 cur.execute("UPDATE exchanges SET  time_to = %s, fee_to = %s WHERE id = %s", (transaction["received"].replace("T", " ")[:-5], (transaction["fees"] / 100000000), exchange_id))
             elif currency == "LTC":
-                transaction = requests.get("https://api.blockcypher.com/v1/ltc/main/txs/" + tx_hash).json()
+                transaction = requests.get("https://api.blockcypher.com/v1/ltc/main/txs/" + str(tx_hash)).json()
                 cur.execute("UPDATE exchanges SET  time_to = %s, fee_to = %s WHERE id = %s", (transaction["received"].replace("T", " ")[:-5], (transaction["fees"] / 100000000), exchange_id))
             elif currency == "ETH Infura":
-                transaction = requests.get("https://api.infura.io/v1/jsonrpc/mainnet/eth_getTransactionByHash?params=%5B%22" + tx_hash + "%22%5D&token=Wh9YuEIhi7tqseXn8550").json()["result"]
+                transaction = requests.get("https://api.infura.io/v1/jsonrpc/mainnet/eth_getTransactionByHash?params=%5B%22" + str(tx_hash) + "%22%5D&token=Wh9YuEIhi7tqseXn8550").json()["result"]
                 block = requests.get("https://api.infura.io/v1/jsonrpc/mainnet/eth_getBlockByNumber?params=%5B%22" + str(transaction["blockNumber"]) + "%22%2C%20true%5D&token=Wh9YuEIhi7tqseXn8550").json()["result"]
                 cur.execute("UPDATE exchanges SET  time_to = %s, fee_to = %s WHERE id = %s", (datetime.datetime.utcfromtimestamp(int(block["timestamp"], 16)).strftime('%Y-%m-%d %H:%M:%S'), int(transaction["gas"], 16)*(int(transaction["gasPrice"], 16) / 1E+18), exchange_id))
             db.commit()
